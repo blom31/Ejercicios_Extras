ed74b5f1336811c16c8f9fef4804e3ae2d7e8c33
Olga Betancourt <blom31@gmail.com>
2023-03-31 13:29:58 -0300

    4to commit

--- src/ejercicios_extras/ejercicio_11.java
+++ src/ejercicios_extras/ejercicio_11.java
@@ -1,3 +1,5 @@
+package ejercicios_extras;
+
 /*Escribir un programa que lea un número entero y devuelva el número de dígitos
 que componen ese número. Por ejemplo, si introducimos el número 12345, el programa deberá devolver 5.
 Calcular la cantidad de dígitos matemáticamente utilizando el operador de división. 
